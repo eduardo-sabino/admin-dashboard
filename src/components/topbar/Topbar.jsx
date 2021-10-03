999561885193ece4b8a02da85c2d02b26d90dbd6
Eduardo Guerra Sabino <sabino.eduardo.guerra@gmail.com>
2021-10-03 21:46:17 +0100

    feat: add icons to navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,28 @@
 import "./topbar.css"
-import { NotificationsNoneIcon } from '@mui/icons-material';
+import {NotificationsNone, Language, Settings} from '@mui/icons-material/';
+/* import LanguageIcon from '@mui/icons-material/Language'; */
+/* import { NotificationsNone } from '@material-ui/icons/'; */
 
 export default function Topbar(){
   return (
-    <div className='topbar'>
-      <div className='topbar-wrapper'>
-        <div className='topleft'>
+    <div className='top-bar'>
+      <div className='top-bar-wrapper'>
+        <div className='top-left'>
           <span className='logo'>Dashboard</span>
         </div>
-        <div className='topright'>
-          <div className="topbar-icons">
-            <NotificationsNoneIcon />
+        <div className='top-right'>
+          <div className="top-bar-icon-container">
+            <NotificationsNone className='top-bar__icon' />
+            <span className="top-icon-badge">2</span>
           </div>
+          <div className="top-bar-icon-container">
+            <Language className='top-bar__icon' />
+            <span className="top-icon-badge">2</span>
+          </div>
+          <div className="top-bar-icon-container">
+            <Settings className='top-bar__icon' />
+          </div>
+          <div className="top-avatar"></div>
         </div>
       </div>
     </div>
